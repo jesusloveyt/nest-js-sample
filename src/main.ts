0ceb1d6ff07092104fd929dde66777c97d7a320b
ytlim <jesusloveyt@gmail.com>
2025-05-31 15:42:08 +0900

    hide password

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +10,7 @@ async function bootstrap() {
       excludeExtraneousValues: true,
     },
   }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get('Reflector')));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
